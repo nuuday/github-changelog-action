c203d27c4261b6f7106448ec8e1569a306e9f75f
Nicolai Willems <172633+nwillems@users.noreply.github.com>
2021-05-26 10:33:54 +0200

    Add more debug info
    After experiencing some issues, we saw a need to have better debug information

--- entrypoint.sh
+++ entrypoint.sh
@@ -47,9 +47,13 @@ if [ -f "${config}/config.yml" ] && [ -f "${config}/CHANGELOG.tpl.md" ]; then
     echo "${changelog}" > ${output}
   fi
 
+  echo "::debug ::git-chlog: replace %"
   changelog="${changelog//'%'/'%25'}"
+  echo "::debug ::git-chlog: replace new-line"
   changelog="${changelog//$'\n'/'%0A'}"
+  echo "::debug ::git-chlog: replace carriage-return"
   changelog="${changelog//$'\r'/'%0D'}"
+  echo "::debug ::git-chlog: set output"
   echo "::set-output name=changelog::${changelog}"
 
 
